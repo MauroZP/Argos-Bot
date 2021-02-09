6c938e5f465a796e19eee65f406894806cf01448
MauroZP <mews@live.com.mx>
2021-02-09 05:41:53 +0000

    Keeping alive the server method

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import discord
 import os
 import requests
 import json
+from keep_alive import keep_alive
 
 client = discord.Client()
-
-
 
 @client.event
 async def on_ready():
@@ -19,4 +18,5 @@ async def on_message(message):
   if message.content.startswith('$Hello'):
     await message.channel.send('Hi')
 
+keep_alive()
 client.run(os.getenv('TOKEN'))
