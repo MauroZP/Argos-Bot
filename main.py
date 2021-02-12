8b3ed9bd4b850848967be0e25402e005c8c730ae
Maurr <mews@live.com.mx>
2021-02-12 02:38:29 +0000

    Adding commands

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import discord
+from discord.ext import commands
 import os
-import requests
-import json
 from keep_alive import keep_alive
 
-client = discord.Client()
+# DIFFERENT TYPE
+#client = discord.Client()
+client = commands.Bot(command_prefix = ">")
 
 @client.event
 async def on_ready():
   print('Logged {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-  if message.author == client:
-    return
-  
-  if message.content.startswith('$Hello'):
-    await message.channel.send('Hi')
+# FROM DIFFERENT TYPE
+#@client.event
+#async def on_message(message):
+  #if message.author == client:
+  #  return
+  #if message.content.startswith('$Hello'):
+  #  await message.channel.send('Hi')
+
+@client.command()
+async def play(ctx, url : str):
+  voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
+  voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+  await voiceChannel.connect()
+
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
